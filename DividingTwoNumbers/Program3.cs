5c2cda3b959529bf6c8b4c59be68a1be3f403aa8
maSta89 <hastikj@seznam.cz>
2021-03-31 19:02:45 +0200

    new program3

---
+++ DividingTwoNumbers/Program3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DividingTwoNumbers {
+    class Program3 {
+        static void Main(string[] args) {
+            double number1 = 15;
+            double number2 = 6;
+            double div;
+            div = number1 / number2;
+            Console.WriteLine(div);
+        }
+    }
+}
