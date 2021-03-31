d78d79dcfa73e9dc69df5dcafae5d4f86b501859
maSta89 <hastikj@seznam.cz>
2021-03-31 18:48:24 +0200

    program desription comment added

--- HelloName/Program1.cs
+++ HelloName/Program1.cs
@@ -2,7 +2,7 @@
 
 namespace HelloName {
     class Program1 {
-        static void Main(string[] args) {
+        static void Main(string[] args) { //Write a C# Sharp program to print Hello and your name in a separate line.
             Console.WriteLine("Hello");
             Console.WriteLine("George maSta89");
         }
--- SumTwoNumbers/Program2.cs
+++ SumTwoNumbers/Program2.cs
@@ -2,7 +2,7 @@
 
 namespace SumTwoNumbers {
     class Program2 {
-        static void Main(string[] args) {
+        static void Main(string[] args) { //Write a C# Sharp program to print the sum of two numbers.
             int number1 = 7;
             int number2 = 3;
             int sum;
