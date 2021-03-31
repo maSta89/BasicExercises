9e863a39d37509b0d7b658ccafe93edb76fbc76c
maSta89 <hastikj@seznam.cz>
2021-03-31 18:23:29 +0200

    Add project files.

---
+++ HelloName/Program1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HelloName {
+    class Program1 {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello");
+            Console.WriteLine("George maSta89");
+        }
+    }
+}
---
+++ SumTwoNumbers/Program1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SumTwoNumbers {
+    class Program1 {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
