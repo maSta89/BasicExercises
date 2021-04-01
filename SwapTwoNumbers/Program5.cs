6dc2cdd405ed2a857644833db8ec11cf67d12c61
maSta89 <hastikj@seznam.cz>
2021-04-01 12:20:04 +0200

    Adden program for swaping numbers (usecase filtering and comparison)

---
+++ SwapTwoNumbers/Program5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SwapTwoNumbers_ {
+    class Program5 {
+        static void Main(string[] args) { //Write a C# Sharp program to swap two numbers.
+
+            int midstep;
+
+            Console.WriteLine("enter first number:");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter second number:");
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("first number: "+number1);
+            Console.WriteLine("second number: "+number2);
+
+            Console.WriteLine("Swaped:");
+
+            midstep = number1;
+            number1 = number2;
+            number2 = midstep;
+
+            Console.WriteLine("first number: " + number1);
+            Console.WriteLine("second number: " + number2);
+
+
+        }
+    }
+}
