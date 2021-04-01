c87276b14e5266bda3dce4b192d173cf6ed9f50d
maSta89 <hastikj@seznam.cz>
2021-04-01 13:22:23 +0200

    Program 7 math operation with input from user

---
+++ MathOperUserImput/Program7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MathOperUserImput {
+    class Program7 {
+        static void Main(string[] args) {
+            //Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing of two numbers
+            //which will be entered by the user.
+
+            Console.WriteLine("enter first number:");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter second number:");
+            double number2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Result for adding two number {number1} and {number2} equals {number1+number2}");
+
+            Console.WriteLine($"Result for subtracting two number {number1} and {number2} equals {number1 - number2}");
+
+            Console.WriteLine($"Result for multiplying two number {number1} and {number2} equals {number1 * number2}");
+
+            Console.WriteLine($"Result for dividing two number {number1} and {number2} equals {number1 / number2}");
+        }
+    }
+}
