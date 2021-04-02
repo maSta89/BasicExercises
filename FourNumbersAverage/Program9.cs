67ec0fc50d016390a71fc38f8ba6a11f995993f8
maSta89 <hastikj@seznam.cz>
2021-04-02 11:45:00 +0200

    use of ARRAY and FOR loop to calculate average

---
+++ FourNumbersAverage/Program9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FourNumbersAverage {
+    class Program9 {
+        static void Main(string[] args) { // Write a C# Sharp program that takes four numbers as input to calculate and print the average.
+
+            int[] Arraynumber = new int[4];
+
+            double sum = 0;
+
+            for (int i = 0; i < Arraynumber.Length; i++) {
+
+                Console.WriteLine("insert number: " + (i + 1));
+                Arraynumber[i] = int.Parse(Console.ReadLine());
+                sum = sum + Arraynumber[i];
+            }
+
+            double avg;
+            avg = sum / Arraynumber.Length;
+
+            Console.WriteLine("average for your numbers equals to: "+avg);
+        }
+    }
+}
