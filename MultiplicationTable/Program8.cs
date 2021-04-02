96231df07c34d489dd15809b1e90c6426149d5fe
maSta89 <hastikj@seznam.cz>
2021-04-02 10:59:15 +0200

    firt use of FOR cycle

---
+++ MultiplicationTable/Program8.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MultiplicationTable {
+    class Program8 {
+        static void Main(string[] args) { //Write a C# Sharp program that takes a number as input and print its multiplication table.
+
+            Console.WriteLine("enter first number:");
+            int number1 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++) {
+                Console.WriteLine(number1*i);
+            }
+        }
+    }
+}
