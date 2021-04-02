f944c428bedf46130858017706bf6fbddd2db250
maSta89 <hastikj@seznam.cz>
2021-04-02 13:25:46 +0200

    Program 10 two formulas

---
+++ ThreeNumbersFormula/Program10.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ThreeNumbersFormula {
+    class Program10 {
+        static void Main(string[] args) {//Write a C# Sharp program to that takes three numbers(x,y,z) as input and print the output of (x+y).z and x.y + y.z.
+
+            Console.WriteLine("enter x number:");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter y number:");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter z number:");
+            double z = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"first formula (x+y)*z = {(x+y)*z}");
+
+            Console.WriteLine($"second formula x*y + y*z = {x * y + y * z}");
+
+        }
+    }
+}
