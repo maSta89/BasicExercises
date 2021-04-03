1c665cf4829d072a8aa1d4434df55195d363bb8f
maSta89 <hastikj@seznam.cz>
2021-04-03 23:31:29 +0200

    Temperature converter

---
+++ ConvertCeltoKeltoFahr/Program14.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConvertCeltoKeltoFahr {
+    class Program14 {
+        static void Main(string[] args) {//Write a C# program to convert from celsius degrees to Kelvin and Fahrenheit.
+
+            Console.Write("enter degrees:");
+            double cel = int.Parse(Console.ReadLine());
+
+            double kel = cel + 273.15;
+            Console.WriteLine($"Your temperature {cel}°C equals to {kel}°K ");
+
+            double fehr = (cel * 1.8000)+32;
+            Console.WriteLine($"Your temperature {cel}°C equals to {fehr}°F ");
+
+        }
+    }
+}
