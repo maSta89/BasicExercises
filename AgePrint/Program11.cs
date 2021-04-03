e7799022a9760c3a8012e14d3f213258d1f27181
maSta89 <hastikj@seznam.cz>
2021-04-03 19:43:16 +0200

    Program 11 age insult

---
+++ AgePrint/Program11.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AgePrint {
+    class Program11 {
+        static void Main(string[] args) {//Write a C# Sharp program that takes an age (for example 20) as input and prints something as "You look older than 20".
+
+            Console.WriteLine("Insert your age");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"You look older than {age}");
+        }
+    }
+}
