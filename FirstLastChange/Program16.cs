6d64949a77172fef8abfa4659f517ed3e22b7bba
maSta89 <hastikj@seznam.cz>
2021-04-05 00:22:32 +0200

    Switch first and last chracter of given word

---
+++ FirstLastChange/Program16.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FirstLastChange {
+    class Program16 {
+        static void Main(string[] args) {
+            //Write a C# program to create a new string from a given string where the first and last characters will change their positions.
+
+            Console.WriteLine("Write your world");
+            string word = Console.ReadLine();
+
+            char firstcharac = word[0];
+            char lastcharac = word[word.Length-1];
+
+            char[] ArrayWord = new char[word.Length];
+
+            for (int i = 0; i < word.Length; i++) {
+                ArrayWord[i] = word[i];
+            }
+
+            ArrayWord[0] = lastcharac;
+            ArrayWord[word.Length-1] = firstcharac;
+
+            Console.WriteLine(ArrayWord);
+        }
+    }
+}
