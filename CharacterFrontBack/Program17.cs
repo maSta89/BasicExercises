6cba39110e85fbf52d1c68e063a234e3bfbea2e5
maSta89 <hastikj@seznam.cz>
2021-04-05 01:04:29 +0200

    Put fisrt character to front and back

---
+++ CharacterFrontBack/Program17.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CharacterFrontBack {
+    class Program17 {
+        static void Main(string[] args) {
+            //Write a C# program to create a new string from a given string (length 1 or more ) with the first character added at the front and back.
+
+            Console.WriteLine("Write your sentence from one character to multiple words.");
+            string word = Console.ReadLine();
+
+            char firstLastChar = word[0];
+
+            Console.WriteLine(firstLastChar+word+firstLastChar);
+        }
+    }
+}
