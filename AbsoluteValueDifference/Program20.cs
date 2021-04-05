ef265970f3278e40b5a0c664f241fa889f513135
maSta89 <hastikj@seznam.cz>
2021-04-05 10:49:37 +0200

    use of absolute method of math class

---
+++ AbsoluteValueDifference/Program20.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AbsoluteValueDifference {
+    class Program20 {
+        static void Main(string[] args) {
+            //Write a C# program to get the absolute value of the difference between two given numbers.
+            //Return double the absolute value of the difference if the first number is greater than second number.
+
+            Console.WriteLine("enter first number positive or negative");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter first number positive or negative");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int difference;
+
+            difference = number1 - number2;
+
+            if (number1 < number2) {
+                Console.WriteLine(Math.Abs(difference));
+            }
+            else if (number1 > number2) {
+                Console.WriteLine(Math.Abs(difference*2));
+            }
+            else {
+                Console.WriteLine("numbers are equal");
+            }
+        }
+    }
+}
