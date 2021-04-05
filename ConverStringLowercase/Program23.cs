98f46d39ad2bf54d855df07d788dc8969bc7d634
maSta89 <hastikj@seznam.cz>
2021-04-05 18:38:16 +0200

    converison of random string to lowercase characters

---
+++ ConverStringLowercase/Program23.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConverStringLowercase {
+    class Program23 {
+        static void Main(string[] args) {//Write a C# program to convert a given string into lowercase.
+
+            Console.WriteLine("Write a word with at least one upper character");
+            string word = Console.ReadLine();
+
+            Console.WriteLine($"conversion of your word \n {word} \n is {word.ToLower()}");
+        }
+    }
+}
