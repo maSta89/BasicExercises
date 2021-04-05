471dce1f323693baf0f7d67f1c6574754e920f51
maSta89 <hastikj@seznam.cz>
2021-04-05 10:28:34 +0200

    if inputs equals print triple of sum value

---
+++ TwoNumbersTriple/Program19.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TwoNumbersTriple {
+    class Program19 {
+        static void Main(string[] args) {
+            //Write a C# program to compute the sum of two given integers,
+            //if two values are equal then return the triple of their sum.
+
+            Console.WriteLine("enter first number positive or negative");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter first number positive or negative");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int outcome;
+            if (number1==number2) {
+                outcome = (number1 + number2) * 3;
+            }
+            else {
+                outcome = (number1 + number2);
+            }
+            Console.WriteLine(outcome);
+        }
+    }
+}
