6dec55cd19fc7daecac5f8774957e5a83f2cca8a
maSta89 <hastikj@seznam.cz>
2021-04-06 13:28:12 +0200

    find longest word in sentence

---
+++ LongestWord/Program24.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LongestWord {
+    class Program24 {
+        static void Main(string[] args) {//Write a C# program to find the longest word in a string.
+
+            Console.WriteLine("Write sentence with multiple words");
+            string sentence = Console.ReadLine();
+
+            String[] words = sentence.Split(' ', '.', ',');
+
+            int maxlenght = 0;
+
+            string longest = "";
+
+            for (int i = 0; i < words.Length; i++) {
+                if (words[i].Length > maxlenght) {
+                    maxlenght = words[i].Length;
+                    longest = words[i];
+                }
+            }
+
+            Console.WriteLine(longest);
+
+        }
+    }
+}
