46bc8bdec973aec2d837a6b5cc59adcfdfc014ef
maSta89 <hastikj@seznam.cz>
2021-04-06 14:46:20 +0200

    Print out all odd numbers from 1 to 99

---
+++ OddNumbersTo99/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OddNumbersTo99 {
+    class Program {
+        static void Main(string[] args) {
+            //Write a C# program to print the odd numbers from 1 to 99. Prints one number per line.
+
+            int nModulo=0;
+
+            for (int i = 1; i < 100; i++) {
+                for (int ind = 1; ind <= i; ind++) {
+                    if (i%ind == 0) {
+                        nModulo++;
+                    }
+                }
+                if (nModulo == 2) {
+                    Console.WriteLine(i);
+                }
+                nModulo = 0;
+            }
+        }
+    }
+}
