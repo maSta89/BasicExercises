aca6219004bdad5b77a65e8b61723f8c3d8d5eab
maSta89 <hastikj@seznam.cz>
2021-04-06 15:24:53 +0200

    Sum of first 500 prime numbers

---
+++ 26.Sum500PrimeNumbers/Program26.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _26.Sum500PrimeNumbers {
+    class Program26 {
+        static void Main(string[] args) {
+            //Write a C# program to compute the sum of the first 500 prime numbers.
+
+            int nModulo = 0;
+            int sum = 0;
+
+            int alfa = 1;
+
+            int primeN=0;
+            for (int i = 1; i <= alfa; i++) {
+
+                alfa++;
+
+                for (int ind = 1; ind <= i; ind++) {
+                    if (i % ind == 0) {
+                        nModulo++;
+                    }
+                }
+                if (nModulo == 2) {
+                    sum = sum + i;
+                    primeN++;
+                    //Console.WriteLine(i);
+                }
+                nModulo = 0;
+                if (primeN == 500) {
+                    break;
+                }
+
+
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
