c18009f61e5570dceb007124b239b4865fe75bbf
maSta89 <hastikj@seznam.cz>
2021-04-07 18:49:59 +0200

    Reverse sentence

---
+++ ReverseSentenceWords/Program28.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _28.ReverseSentenceWords {
+    class Program28 {
+        static void Main(string[] args) {//Write a C# program to reverse the words of a sentence.
+
+            Console.WriteLine("Write sentence to reverse");
+
+            string sentence = Console.ReadLine();
+
+            string[] wordsArray = sentence.Split(' ', '.');
+
+            string sentenceRev="";
+
+            for (int i = wordsArray.Length-1; i >= 0; i--) {
+                sentenceRev = sentenceRev + wordsArray[i] +" ";
+            }
+
+            Console.WriteLine(sentenceRev);
+
+        }
+    }
+}
