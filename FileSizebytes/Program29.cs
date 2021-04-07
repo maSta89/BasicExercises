ccbfc14459f46adc2cd320055e7efcde7142a4bc
maSta89 <hastikj@seznam.cz>
2021-04-07 20:44:35 +0200

    check for file size

---
+++ FileSizebytes/Program29.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace _29.FileSizebytes {
+    class Program29 {
+        static void Main(string[] args) {//Write a C# program to find the size of a specified file in bytes.
+
+            FileInfo file = new FileInfo(@"D:\programovani kurz\cviceniw3resource\BasicExercises\FileSizebytes\Files\File1.txt"); //File is attachted to project
+            Console.WriteLine("File size is: "+ file.Length.ToString() + " byte");
+        }
+    }
+}
