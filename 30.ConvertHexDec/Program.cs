799c98f60dea4f39b0da2d050c08ddda1c829bb4
maSta89 <hastikj@seznam.cz>
2021-04-19 17:43:06 +0200

    Conversion fro hexidecimal numbers

---
+++ 30.ConvertHexDec/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _30.ConvertHexDec {
+    class Program {
+        static void Main(string[] args) {
+            //Write a C# program to convert a hexadecimal number to decimal number.
+
+            Console.WriteLine("write hexidecimal number");
+            string strhex = Console.ReadLine();
+            int hex = Convert.ToInt32(strhex, 16);
+            Console.WriteLine(hex);
+        }
+    }
+}
