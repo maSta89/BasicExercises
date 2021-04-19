5e1a2666376f91144bc84cb6c1b5b626a60bfb3c
maSta89 <hastikj@seznam.cz>
2021-04-20 00:52:02 +0200

    Is given number multiple of 3 , 7?

---
+++ 33. Multiple3or7/Program33.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _33._Multiple3or7 {
+    class Program33 {
+        static void Main(string[] args) {
+            //Write a C# program to check if a given positive number is a multiple of 3 or a multiple of 7.
+
+            Console.WriteLine("write a positive number" );
+            int number = int.Parse(Console.ReadLine());
+
+            if ((number % 3) == 0 && (number % 7) == 0) {
+                Console.WriteLine("number is multiple of 3 and 7");
+            }
+            else if ((number % 3) == 0 && number !=0) {
+                Console.WriteLine("number is multiple of 3");
+            }
+            else if ((number % 7)  == 0 && number != 0) {
+                Console.WriteLine("number is multiple of 7");
+            }
+            else {
+                Console.WriteLine("number is not multiple of 3 or 7");
+            }
+        }
+    }
+}
