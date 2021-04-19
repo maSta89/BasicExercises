6192ba14fe91c948157ae382fb18be25df9ec935
maSta89 <hastikj@seznam.cz>
2021-04-19 18:07:00 +0200

    Multiplying of two arrays

---
+++ 31.MultiplyArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _31.MultiplyArrays {
+    class Program {
+        static void Main(string[] args) {
+            //Write a C# program to multiply corresponding elements of two arrays of integers.
+
+            int[] array1 = { 1,3,-5,4};
+            int[] array2 = { 1,4,-5,-2};
+
+            int multiply;
+            string outputmultiply="";
+
+            for (int i = 0; i < array1.Length; i++) {
+
+                multiply= array1[i] * array2[i];
+                outputmultiply = outputmultiply + multiply + " ";
+            }
+
+            Console.WriteLine(outputmultiply);
+        }
+    }
+}
