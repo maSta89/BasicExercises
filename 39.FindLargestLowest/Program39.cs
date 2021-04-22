ae5dc1082ed1b9cdffdc0b8d9504569e35b80321
maSta89 <hastikj@seznam.cz>
2021-04-22 13:57:15 +0200

    From three given numbers sellect largest and lowest

---
+++ 39.FindLargestLowest/Program39.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _39.FindLargestLowest {
+    class Program39 {
+        static void Main(string[] args) {
+            //Write a C# program to find the largest and lowest values from three integer values
+
+            Console.WriteLine("Input three numbers");
+
+            List<int> Numlist = new List<int>();
+
+            Console.WriteLine("first");
+            Numlist.Add(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("second");
+            Numlist.Add(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("third");
+            Numlist.Add(int.Parse(Console.ReadLine()));
+
+            Numlist.Sort();
+
+            int [] numarray =  Numlist.ToArray();
+
+            Console.WriteLine("highest number is = " + numarray.GetValue(numarray.Length-1));
+
+        }
+    }
+}
